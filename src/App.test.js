9a186971fb01ea425e64a80c117b53c291cb9689
Lucas Dramowicz <lucas.dramovicz@desjardins.com>
2019-03-07 22:29:04 -0500

    Test App > should render a <div /> = failed

--- src/App.test.js
+++ src/App.test.js
@@ -10,4 +10,8 @@ describe('App', () => {
         ReactDOM.render(<App />, div);
         ReactDOM.unmountComponentAtNode(div);
     });
+    it('should render a <div />', () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.find('div').length).toEqual(1);
+    });
 });
