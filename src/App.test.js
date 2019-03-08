b5e6b2d0dd268838d5d798980e6ec1eabb36b4e6
Lucas Dramowicz <lucas.dramovicz@desjardins.com>
2019-03-07 23:16:44 -0500

    Test App > should render the Board Component = failed

--- src/App.test.js
+++ src/App.test.js
@@ -5,13 +5,20 @@ import '../setupTests';
 import App from './App';
 
 describe('App', () => {
-    it('renders without crashing', () => {
+    let wrapper;
+    beforeEach(() => wrapper = shallow(<App />));
+
+    it('should render without crashing', () => {
         const div = document.createElement('div');
         ReactDOM.render(<App />, div);
         ReactDOM.unmountComponentAtNode(div);
     });
+
     it('should render a <div />', () => {
-        const wrapper = shallow(<App />);
         expect(wrapper.find('div').length).toEqual(1);
+    });
+
+    it('should render the Board Component', () => {
+        expect(wrapper.containsMatchingElement(<Board />)).toEqual(true);
     });
 });
